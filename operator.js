59e96871c2f7a7b5bc9966476333d4fd4d5d75db
Jinho94 <ljinho1001@gmail.com>
2021-01-29 02:19:43 +0900

    operator, if, for loop - 2

--- operator.js
+++ operator.js
@@ -10,8 +10,8 @@ console.log(1 + 1); //add
 console.log(1 - 1); //substractyd
 console.log(1 / 1); //divide
 console.log(1 * 1); //multiply
-console.log(1 % 1); //remainer(몫)
-console.log(1 ** 1); //exponentiation
+console.log(5 % 2); //remainer(나누고 나머지값)
+console.log(2 ** 3); //exponentiation
 
 // 3. Increment and decrement operators
 let counter = 2;
@@ -25,12 +25,12 @@ const postIncrement = counter++;
 // counter = counter + 1;  4
 console.log(`postIncrement: ${postIncrement}, counter: ${counter}`);
 
-const preDecrement = ++counter; //[선업데이트, 후할당]
+const preDecrement = --counter; //[선업데이트, 후할당]
 // counter = counter +1; 카운터에 1을 더해서 다시 할당, counter 값은 3
 // preDecrement = counter preDecrement에 카운터를 할당, 즉 3이 할당됨
 console.log(`preDecrement: ${preDecrement}, counter: ${counter}`);
 
-const postDecrement = counter++;
+const postDecrement = counter--;
 // postDecrement = counter; 현재 counter는 3, 따라서 post도 3 [선할당, 후업데이트]
 // counter = counter + 1;  4
 console.log(`postDecrement: ${postDecrement}, counter: ${counter}`);
@@ -81,4 +81,137 @@ function check(){
 console.log(!value1);
 
 // 7. Equality
-const 
+const stringFive = '5';
+const numberFive = 5;
+
+// ==loose equality, with type conversion
+console.log(stringFive == numberFive);
+console.log(stringFive != numberFive);
+
+// ===strict equality, no type conversion , type이 같이 않으면 동일하지 않음.
+console.log(stringFive === numberFive);
+console.log(stringFive !== numberFive);
+
+//object equality be reference
+const ellie1 = { name: 'ellie' };
+const ellie2 = { name: 'ellie' };
+const ellie3 = ellie1;
+//1과 2는 똑같은 메모리가 들어있는 object지만 각각 다른 reference가 들어있음
+
+console.log(ellie1 == ellie2);
+console.log(ellie1 === ellie2);
+console.log(ellie1 === ellie3);
+
+//equality - puzzler
+console.log(0 == false);
+console.log(0 === false);
+console.log('' == false);
+console.log('' === false);
+console.log(null == undefined);
+console.log(null === undefined);
+
+// 8. Conditional operators: if
+// if, else if, else
+const name = 'ellie';
+if (name === 'ellie') {
+	console.log('Welcome, Ellie!');
+} else if (name === 'coder') {
+	console.log('you are amazing coder');
+} else {
+	console.log('unknown');
+}
+
+// 9. Ternary operator: ?
+// condition ? value1 : value2;
+console.log(name === 'ellie' ? 'yes' : 'no');
+
+// 10. Switch statement
+// use for multiple if checks
+// use for enum-like value check
+// use for multiple type checks in TS
+
+const browser = 'IE';
+switch (browser) {
+	case 'IE':
+		console.log('go away!');
+		break;
+	case 'Chrome':
+	case 'Firefox':
+		console.log('love you!');
+		break;
+	default:
+		console.log('same all!');
+		break;
+}
+
+// 11. Loops
+// while loop, while the condition is truthy, false가 나올때까지 계속 반복함.
+// body code is executed.
+let i = 3;
+while (i > 0) {
+	console.log(`while: ${i}`);
+	i--;
+}
+
+// do while loop, body code is executed first, //block을 먼저 실행하고 나서 조건 확인
+// then check the condition.
+do {
+	console.log(`do while: ${i}`);
+	i--;
+} while (i > 0);
+
+// for loop, for(begin; consition; step)
+for (i = 3; i > 0; i--) {
+	console.log(`for: ${i}`);
+}
+
+for (let i = 3; i > 0; i = i - 2) {
+	//inline variable declaration
+	console.log(`inline variable for: ${i}`);
+}
+
+//nested loops
+for (let i = 0; i < 10; i++) {
+	for (let j = 0; j < 10; j++) {
+		console.log(`i:${i}, j:${j}`);
+	}
+}
+
+// break, continue
+// Q1. iterate from 0 to 10 and print only even numbers(use continue)
+
+{ //mine
+	const a = 0;
+	while (a =< 10) {
+		i++;
+		if (a == even) {
+			console.log(`while: ${a}`);
+		}	
+	}
+}
+
+//Answer
+	for (let a = 0; a < 11; a++) {
+		if (a % 2 === 0) {
+			console.log(`q1. ${a}`);
+		}
+	}
+
+// Q2. iterate from 0 to 10 and print numbers until reaching 8 (use break)
+
+{ //mine
+	const b = 0;
+	if (b < 8) {
+		console.log(`while: ${b}`);
+		i++;
+	} else {
+		break;
+	}
+}
+//Answer
+for (let b = 0; b < 11; b++){
+	if (b > 8) {
+		break;
+	}
+	console.log(`q2. ${b}`);
+}
